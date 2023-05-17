a6721aaf92daedfd252352637e0cdf7d3d5defaa
HUAWEI <rubenovnanna@gmail.com>
2023-05-17 22:08:39 +1000

    Design + app icon

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <string>
 
+
 /*
     We can just use json::array_t to store parsed info about boards and users instead of convering it to std::vector  (Done)
     Not sure about json -> std::string -> qstring convertion, may be an issue
@@ -27,6 +28,7 @@ int main(int argc, char *argv[])
 //    Database::ParseBoardsData();
 //    Database::DeleteBoard("Board1");
 //    return 1;
+
 
 
     Database::ParseBoardsData();
--- task_tracker_qt.pro
+++ task_tracker_qt.pro
@@ -9,6 +9,7 @@ CONFIG += c++17
 # In order to do so, uncomment the following line.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
+
 SOURCES += \
     Database\database.cpp \
     UI/w_cell/cell.cpp \
@@ -45,3 +46,8 @@ else: unix:!android: target.path = /opt/$${TARGET}/bin
 
 DISTFILES += \
     text.txt
+
+RESOURCES += \
+    other/resource.qrc
+
+RC_FILE += "other/test.rc"
