6ade6b4b35b0761f179b40f7c8b9fa74a034206f
Vasiliy Davydov <davvvas@mail.ru>
2023-05-17 13:34:15 +1000

    something

--- UI/w_reg/registration.cpp
+++ UI/w_reg/registration.cpp
@@ -3,6 +3,7 @@
 #include "Database/database.h"
 
 #include <QMessageBox>
+#include <QRegularExpression>
 
 Registration::Registration(QWidget *parent) :
     QDialog(parent),
@@ -21,23 +22,37 @@ void Registration::on_btn_SignUp_pressed()
     QString Login = ui->lineEdit_Login->text();
     QString Password = ui->lineEdit_Password->text();
 
+    //std::regex pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]");
+    //bool hasLetters = Login.contains(QRegularExpression("[A-Za-z]"));
+    //bool hasDigits = Login.contains(QRegularExpression("\\d"));
+    //bool hasSpecialChars = Login.contains(QRegularExpression("[-_*#$@&?!]"));
 
     if (Login.isEmpty() or Password.isEmpty()) {
 
         QMessageBox::warning(this, "Incorrect input", "Entered data is not valid, empty, or contains prohibited symbols: _ _ _ _ _ _");
 
     } else {
-
-        int SuccesfullyAdded = Database::AddUser(Login, Password);
-
-        if (SuccesfullyAdded) {
-
-            QMessageBox::information(this, "кайф", "Успешно зарегистрировался");
-
-        } else {
-
-            QMessageBox::warning(this, "печалька", "Занято, попутал ты чето");
+        if (((Login.length() < 4) or (Login.length() > 15)) or ((Password.length() < 4) or (Password.length() > 15))) {
+            QMessageBox::information(this, "печалька", "Минимум - 4 символа, максимум - 15");
         }
+        else {
+            if(!Password.contains(QRegularExpression("[a-zA-Z0-9_-*#$@&?!]+")) or !Login.contains(QRegularExpression("[]+"))) {
+                QMessageBox::information(this, "печалька", "Поле должно содержать только буквы, цифры и _ - * # $ @ & ? !");
+            }
+            else {
+                int SuccesfullyAdded = Database::AddUser(Login, Password);
+
+                if (SuccesfullyAdded) {
+
+                    QMessageBox::information(this, "кайф", "Успешно зарегистрировался");
+
+                } else {
+
+                    QMessageBox::warning(this, "печалька", "Занято, попутал ты чето");
+                }
+            }
+        }
+
     }
 }
 
